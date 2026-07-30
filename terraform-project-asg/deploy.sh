f715d1e0c80ad29dff287ac1f59c63848eefe33f
pranay9528 <pkumargoud121@gmail.com>
2026-07-30 23:53:01 +0100

    Fixed deploy script

--- terraform-project-asg/deploy.sh
+++ terraform-project-asg/deploy.sh
@@ -3,6 +3,6 @@ set -e
 echo "starting deployment..."
 cd /home/ubuntu/Devopsreal
 git pull
-sudo cp terraform-project-asg/website/index.html/var/www/html/index.html
+sudo cp terraform-project-asg/website/index.html /var/www/html/index.html
 sudo systemctl restart nginx
 echo "deployment completed"
