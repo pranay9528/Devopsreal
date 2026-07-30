24c5d5b1b52e42f8f602185c15a3bd79790f0514
pranay9528 <pkumargoud121@gmail.com>
2026-07-31 00:33:49 +0100

    Improved Deploy Script

--- terraform-project-asg/deploy.sh
+++ terraform-project-asg/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 echo "starting deployment..."
 cd /home/ubuntu/Devopsreal
+git restore .
 git pull
 sudo cp terraform-project-asg/website/index.html /var/www/html/index.html
 sudo systemctl restart nginx
